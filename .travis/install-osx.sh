b948b2b264d18cf780f12f0ccd9f5562c700ffa9
Maykel Arias Torres <dev0006@simelo.tech>
2019-12-26 11:34:57 -0500

    [travis] refs #190 Using in `OSX` SWIG 4.0 because swig3 not foud commit

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -9,7 +9,7 @@ brew install pyenv-virtualenv;
 brew install readline xz;
 echo 'Creating swig@3.0.12 formula';
 cd "$(brew --repository)/Library/Taps/homebrew/homebrew-core";
-git show 42d31bba7772fb01f9ba442d9ee98b33a6e7a055:Formula/swig.rb | grep -v 'fails_with' > Formula/swig.rb;
+# git show 42d31bba7772fb01f9ba442d9ee98b33a6e7a055:Formula/swig.rb | grep -v 'fails_with' > Formula/swig.rb;
 echo 'Installing swig@3.0.12 (3.0.12)';
 brew install swig || brew link --overwrite swig;
 brew install gimme;
